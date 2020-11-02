144f23739059e6a02392c16c914fc7ffac0699b6
groetjens <65292425+groetjens@users.noreply.github.com>
2020-11-02 15:02:15 +0100

    willekeurig gegenereerde grafiekjes erin. benodigde libaries: numpy, matplotlib, serial
    installeren door in je cmd:
    pip install *naam van library*

--- test_centrale.py
+++ test_centrale.py
@@ -1,5 +1,10 @@
 from tkinter import *
+from functools import partial
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 import serial
+
 
 class Eenheid():
 
@@ -7,14 +12,15 @@ class Eenheid():
         self.name = name
 
 eenheidlijst = []
+
 for i in range(8):
     try:
-        ser = serial.Serial('COM'+str(i), 9600, timeout=0)
+        ser = serial.Serial('COM' + str(i), 9600, timeout=0)
         if isinstance(ser, serial.Serial):
             eenheidlijst.append(ser)
-            print("Aansluiting gevonden op COM"+str(i))
+            print("Aansluiting gevonden op COM" + str(i))
     except:
-        print("Niets aangesloten op port COM"+str(i))
+        print("Niets aangesloten op port COM" + str(i))
 
 
 # eenheid1 = Eenheid("1")
@@ -45,51 +51,71 @@ class Window(Frame):
 
         edit = Menu(menu, tearoff=0)
         for eenheid in eenheidlijst:
-            edit.add_command(label='Remove '+ eenheid.name, command=lambda: self.remove_from_list(eenheid))
+            edit.add_command(label='Remove ' + eenheid.name, command=partial(self.remove_from_list, eenheid))
         menu.add_cascade(label='Edit', menu=edit)
 
         i = 0
-        spacer_begin = Frame(self.master, height=50, width=50)
-        spacer_begin.grid(row=0, column=0)
+        # spacer_begin = Frame(self.master, height=50, width=50)
+        # spacer_begin.grid(row=0, column=0)
 
         for eenheid in eenheidlijst:
+            eenheid_f = Frame(self.master, height=400, width=400)
+            eenheid_f.grid(row=1, column=i+1)
+            spacer_tussen = Frame(eenheid_f, height=50, width=50)
+            spacer_tussen.grid(row=1, column=i * 5)
 
-            spacer_tussen = Frame(self.master, height=50, width=50)
-            spacer_tussen.grid(row=1,column=i*5)
+            fig = Figure(figsize=(6,4), dpi=100, edgecolor="red")
+            fig.add_subplot(111).plot(np.random.normal(8000, 2500, 50))
 
-            graph = Frame(self.master, bg="#d8eded", height=300, width=400)
-            graph.grid(row=1,column=(i*5+1), columnspan=3)
+            canvas = FigureCanvasTkAgg(fig, master=eenheid_f)
+            canvas.draw()
+            canvas.get_tk_widget().grid(row=1, column=(i*5+1), columnspan=3)
 
-            b1 = Button(self.master, text="In-/Uitrollen", padx=10)
-            b1.grid(row=2,column=i*5+1, pady=10)
+            # graph = Frame(eenheid_f, bg="#d8eded", height=300, width=400)
+            # graph.grid(row=1, column=(i * 5 + 1), columnspan=3)
 
-            b2 = Button(self.master, text="Grafiek/Instellingen", padx=10)
-            b2.grid(row=2,column=i*5+2, pady=10)
+            b1 = Button(eenheid_f, text="In-/Uitrollen", padx=10)
+            b1.grid(row=2, column=i * 5 + 1, pady=10)
 
-            b3 = Button(self.master, text=eenheid.name, padx=10)
-            b3.grid(row=2,column=i*5+3, pady=10)
+            b2 = Button(eenheid_f, text="Grafiek/Instellingen", padx=10)
+            b2.grid(row=2, column=i * 5 + 2, pady=10)
 
-            i+=1
+            b3 = Button(eenheid_f, text=eenheid.name, padx=10)
+            b3.grid(row=2, column=i * 5 + 3, pady=10)
+
+            i += 1
 
         spacer_eind = Frame(self.master, height=50, width=50)
         spacer_eind.grid(row=0, column=100)
 
-    ## Dit werkt nog niet helemaal
-
     def remove_from_list(self, eenheid):
-        eenheidlijst.remove(eenheid)
+        global eenheidlijst
+        temp = []
+        for x in eenheidlijst:
+            if x.port != eenheid.port:
+                temp.append(x)
+
+        eenheidlijst = temp
+
         self.clear_frame()
         self.init_window()
 
-    ## Deze werkt ook nog niet helemaal
+        if len(root.grid_slaves()) == 1:
+            self.create_menu()
+        else:
+            pass
+
+
     def clear_frame(self):
-        for widget in self.winfo_children():
-            print(widget)
+        for widget in root.grid_slaves():
             widget.destroy()
-        self.grid_forget()
 
+    def create_menu(self):
+        menu_f = Frame(self.master, height=400)
+        menu_f.grid(row=0, column=0)
 
-
+        menu_button = Button(menu_f, text="Waddup Youtuuub")
+        menu_button.grid(row=2, column=3)
 
 
 
