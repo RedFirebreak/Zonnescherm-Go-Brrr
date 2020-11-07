917dd39de0882cadc0a0e12519269e727c6dc782
groetjens <65292425+groetjens@users.noreply.github.com>
2020-11-07 13:28:44 +0100

    willekeurig gegenereerde grafiekjes erin. benodigde libaries: numpy, matplotlib, serial
    installeren door in je cmd:
    pip install *naam van library*
    
    to do lijst erbij om aan te geven wat er in ieder geval nog moet gebeuren.

--- test_centrale.py
+++ test_centrale.py
@@ -4,6 +4,35 @@ import numpy as np
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import serial
+import webbrowser
+import tkinter.font as tkFont
+import datetime
+
+
+### WAT ER NOG MOET GEBEUREN:
+
+# - Binnengekomen data moet op een bepaalde manier opgeslagen worden zodat het mogelijk is voor matplotlib
+#   om er grafieken van te maken die elke zoveel seconden worden geupdate. Zie deze link:
+#   https://makersportal.com/blog/2018/2/25/python-datalogger-reading-the-serial-output-from-arduino-to-analyze-data-using-pyserial
+
+# - Data voor de grafiek mag maximaal x eenheden zijn, wanneer het x + 1e datapunt binnenkomt zal het
+#   oudste worden verwijderd.
+
+# - Zorgen dat het gehele programma draait en blijft zoeken naar updates in data om deze vervolgens uit te voeren.
+#   Misschien 1 functie die alles verzamelt en dan in één klap de GUI update. Dit kan bij de timer countdown inzitten
+
+# - In plaats van het volledig verwijderen van een besturingseenheid via Edit het mogelijk maken om hem alleen te
+#   verbergen zodat het ook weer terug te halen is.
+
+# - Een label die de status van een eenheid weergeeft. Wanneer je op de knop drukt zal de status automatisch veranderen
+#   (eventueel kunnen we de tijd hoe lang het duurt om een scherm in of uit te rollen aanpassen aan de maximale
+#   uitrol lengte?)
+
+# - De eenheid moet ook data kunnen ontvangen zodat wanneer je op rol in / rol uit drukt de arduino dit binnenkrijgt en
+#   ervoor zorgt dat de lampjes gaan branden zoals het hoort om zo te simuleren dat het zonnescherm bezig gaat.
+
+# - Het type eenheid herkennen aan de hand van de aansluiting. Dit is echter afhankelijk van Stefan en Stefan hoe
+#   zij dit meegeven
 
 
 class Eenheid():
@@ -16,12 +45,30 @@ eenheidlijst = []
 for i in range(8):
     try:
         ser = serial.Serial('COM' + str(i), 9600, timeout=0)
+
         if isinstance(ser, serial.Serial):
             eenheidlijst.append(ser)
-            print("Aansluiting gevonden op COM" + str(i))
-    except:
-        print("Niets aangesloten op port COM" + str(i))
+            #print("Aansluiting gevonden op COM" + str(i))
+        ser.close()
 
+    except:
+        pass
+        #print("Niets aangesloten op port COM" + str(i))
+
+
+# ser = serial.Serial('COM3', 19200)
+# eenheidlijst.append(ser)
+# string = ""
+# run = True
+# while run == True:
+#     s = ser.read()
+#     letter = str(chr(int(s.hex(), 16)))
+#     string = string + letter.rstrip()
+#     if 'KAAS' in string and len(string) > 6:
+#         string = ''
+#     if 'KAAS' == string:
+#         print(string)
+#         run = False
 
 # eenheid1 = Eenheid("1")
 # eenheid2 = Eenheid("2")
@@ -38,6 +85,7 @@ class Window(Frame):
         self.init_window()
 
     def init_window(self):
+        global time_l
         self.master.title("Python Centrale")
 
         menu = Menu(self.master)
@@ -54,6 +102,8 @@ class Window(Frame):
             edit.add_command(label='Remove ' + eenheid.name, command=partial(self.remove_from_list, eenheid))
         menu.add_cascade(label='Edit', menu=edit)
 
+        time_l = Label(self.master, text="", fg='pink', font=tkFont.Font(family="Comic Sans MS", size=30))
+        time_l.grid(row=0, column=0)
         i = 0
         # spacer_begin = Frame(self.master, height=50, width=50)
         # spacer_begin.grid(row=0, column=0)
@@ -80,13 +130,13 @@ class Window(Frame):
             b2 = Button(eenheid_f, text="Grafiek/Instellingen", padx=10)
             b2.grid(row=2, column=i * 5 + 2, pady=10)
 
-            b3 = Button(eenheid_f, text=eenheid.name, padx=10)
+            b3 = Label(eenheid_f, text=eenheid.port, padx=30, pady=3.5, relief="solid", fg="white", bg="pink", font=tkFont.Font(family="Comic Sans MS", size=30))
             b3.grid(row=2, column=i * 5 + 3, pady=10)
 
             i += 1
 
-        spacer_eind = Frame(self.master, height=50, width=50)
-        spacer_eind.grid(row=0, column=100)
+        spacer_einde = Frame(self.master, height=50, width=50)
+        spacer_einde.grid(row=0, column=100)
 
     def remove_from_list(self, eenheid):
         global eenheidlijst
@@ -100,10 +150,8 @@ class Window(Frame):
         self.clear_frame()
         self.init_window()
 
-        if len(root.grid_slaves()) == 1:
+        if len(self.master.grid_slaves()) == 1:
             self.create_menu()
-        else:
-            pass
 
 
     def clear_frame(self):
@@ -114,13 +162,19 @@ class Window(Frame):
         menu_f = Frame(self.master, height=400)
         menu_f.grid(row=0, column=0)
 
-        menu_button = Button(menu_f, text="Waddup Youtuuub")
-        menu_button.grid(row=2, column=3)
+        spacer_links = Frame(menu_f, width=50)
+        spacer_links.grid(row=0,column=0)
+        menu_button = Button(menu_f, text="Waddup Youtuuub, Youtuuuuuuuub", command=partial(webbrowser.open,"https://www.youtube.com/watch?v=CXkG8TOJ2BY"))
+        menu_button.grid(row=1, column=1)
 
-
+# def clock():
+#     time = datetime.datetime.now().strftime("%S")
+#     time_l.config(text=time)
+#     root.after(1000, clock)
 
 root = Tk()
-
 app = Window(root)
+#clock()
+
 
 root.mainloop()
